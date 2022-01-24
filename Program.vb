fad982a477064e32f8d8c51d2d1ad517fdac4dfc
Bk <Bigbk75@gmail.com>
2022-01-24 15:12:00 +0200

    Added barebones functionality

--- Program.vb
+++ Program.vb
@@ -24,14 +24,16 @@ Module Program
         Return Operation.Err
     End Function
     Structure Command
+        Dim c As Char
         Dim op As Operation
         Dim args As String()
         Public Sub New(o As Char, arg As String())
             op = CharacterToOp(o)
             args = arg
+            c = o
 
         End Sub
-        Public Function Exec(q As Queue(Of String)) As String
+        Public Function Exec(ByRef q As Queue(Of String)) As String
             Select Case op
                 Case Operation.Enque
                     Try
@@ -42,24 +44,50 @@ Module Program
                         Return e.Message
                     End Try
                 Case Operation.Deque
-                    q.Dequeue()
+                    Try
+                        q.Dequeue()
+                    Catch e As Exception
+                        Return e.Message
+
+                    End Try
+
+                Case Operation.Reset
+                    Console.WriteLine("Enter the new capacity of the queue")
+                    Dim cap = Integer.Parse(Console.ReadLine())
+                    q = New Queue(Of String)(cap)
+                Case Operation.Err
+
+                    Return $"Unknown command in input '{c}'"
 
             End Select
+            Return ""
+
         End Function
     End Structure
     Dim InstructionDescriptions As String() = {
-        "[D]equeue - Remove an item from the queue",
-        "[E]nqueue - Add an item to the queue",
-        "[R]eset - Empty the queue and enter the maximum capacaity"
+        "[D]equeue  - Remove an item from the queue",
+        "[E]nqueue  - Add an item to the queue",
+        "[R]eset    - Empty the queue and enter the maximum capacaity"
     }
-    Sub Main(args As String())
-        Console.WriteLine("Enter a comma seperated list of instructions from the list e.g E 4, D, E 3, R")
+    Sub Main()
+        Dim Q As New Queue(Of String)
+        Console.WriteLine("Enter a comma seperated list of instructions from the list e.g E 4, D, E 3 2 3 11 1 Hello, R")
         For Each instruction As String In InstructionDescriptions
             Console.WriteLine(instruction)
         Next
         Dim instructions = Console.ReadLine()
-        Dim instructionList = instructions.Split(" ")
-        Dim commands = instructionList.Select(Function(s) New Command(s(0), s.Substring(1).Split(" ")))
+        Dim instructionList = instructions.Split(",")
+        Dim commands = instructionList.Select(Function(s) New Command(s(0), s.Substring(1).Trim().Split(" ")))
+        For Each command As Command In commands
+
+            Dim errorMessage = command.Exec(Q)
+            If errorMessage <> "" Then
+                Console.WriteLine(errorMessage)
+            End If
+
+        Next
+        Console.WriteLine(Q)
+
 
 
 
@@ -105,6 +133,8 @@ Module Program
             _len -= 1
             Return Data(FrontPointer)
         End Function
-
+        Public Overrides Function ToString() As String
+            Return "[" + String.Join(",", Data.Take(_len)) + "]"
+        End Function
     End Class
 End Module
