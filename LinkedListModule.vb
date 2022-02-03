851d2e849b93672f2ba4b1a3e8c8907e659fedb7
Bk the ran man <Bigbk75@gmail.com>
2022-02-03 12:25:44 +0200

    dear god end my undending suffering

---
+++ LinkedListModule.vb
@@ -0,0 +1,28 @@
+﻿Module LinkedListModule
+    Class Node(Of T)
+        Public _next As Integer = -1
+        Dim data As T
+        Sub New(item As T, pointer As Integer)
+            data = item
+            _next = pointer
+        End Sub
+    End Class
+    Class LinkedList(Of T)
+        Dim head As Integer = -1
+        Dim Data(10) As Node(Of T)
+
+        Sub Add(item As T)
+            If head = -1 Then
+                head = 0
+                Data(head) = New Node(Of T)(item, 0)
+            End If
+            Dim _head = head
+
+            While _head < 10 AndAlso Data(head)._next > 0
+                _head = Data(_head)._next
+            End While
+            Data(_head) = New Node(Of T)(item, _head)
+        End Sub
+
+    End Class
+End Module
