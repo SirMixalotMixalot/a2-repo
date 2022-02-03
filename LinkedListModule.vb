95c3166dd85b4e068e2ec9d07fe414f2ac55bdf6
Bk the ran man <Bigbk75@gmail.com>
2022-02-03 12:38:38 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaah

--- LinkedListModule.vb
+++ LinkedListModule.vb
@@ -9,20 +9,26 @@
     End Class
     Class LinkedList(Of T)
         Dim head As Integer = -1
-        Dim Data(10) As Node(Of T)
+
+        Dim _cap As Integer = 10
+        Dim Data(_cap) As Node(Of T)
 
         Sub Add(item As T)
             If head = -1 Then
                 head = 0
                 Data(head) = New Node(Of T)(item, 0)
+                Return
             End If
             Dim _head = head
 
-            While _head < 10 AndAlso Data(head)._next > 0
+            While _head < _cap AndAlso Data(head)._next > 0
                 _head = Data(_head)._next
             End While
             Data(_head) = New Node(Of T)(item, _head)
         End Sub
+        Function Nth(n As Integer) As Node(Of T)
+            Return Data(n)
+        End Function
 
     End Class
 End Module
