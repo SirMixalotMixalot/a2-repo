dd5d0fb56823d44a52ce8599cd8721139fdb8780
Bk <Bigbk75@gmail.com>
2022-02-04 04:48:21 +0200

    it's my mental breakdown and I get to choose the god awful code I write

--- LinkedListModule.vb
+++ LinkedListModule.vb
@@ -2,6 +2,7 @@
     Class Node(Of T)
         Public _next As Integer = -1
         Dim data As T
+
         Sub New(item As T, pointer As Integer)
             data = item
             _next = pointer
@@ -9,22 +10,30 @@
     End Class
     Class LinkedList(Of T)
         Dim head As Integer = -1
-
+        Dim _heapStart As Integer = 0
         Dim _cap As Integer = 10
-        Dim Data(_cap) As Node(Of T)
+        Dim length As Integer = 0
+        Dim lastInsertNext As Integer = 0
+        Dim Data(_cap - 1) As Node(Of T)
 
         Sub Add(item As T)
+
+
             If head = -1 Then
                 head = 0
                 Data(head) = New Node(Of T)(item, 0)
+                lastInsertNext = 1
+                length = 1
                 Return
             End If
-            Dim _head = head
 
-            While _head < _cap AndAlso Data(head)._next > 0
-                _head = Data(_head)._next
-            End While
-            Data(_head) = New Node(Of T)(item, _head)
+            If lastInsertNext = _cap Then
+                ReDim Preserve Data(2 * _cap)
+                _cap *= 2
+            End If
+            Data(lastInsertNext) = New Node(Of T)(item, lastInsertNext + 1)
+            lastInsertNext += 1
+            length += 1
         End Sub
         Function Nth(n As Integer) As Node(Of T)
             Return Data(n)
