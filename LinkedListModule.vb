4b257529bc19ab27bf097e4f63ea62f845c9f986
Bk <Bigbk75@gmail.com>
2022-02-05 21:58:56 +0200

    restarting linked list

--- LinkedListModule.vb
+++ LinkedListModule.vb
@@ -1,5 +1,6 @@
 ﻿Module LinkedListModule
     Class Node(Of T)
+        'Public n As Node(Of T) = Nothing
         Public _next As Integer = -1
         Public data As T
 
@@ -7,54 +8,43 @@
             data = item
             _next = pointer
         End Sub
+        Sub New(item As T)
+            data = item
+            _next = -1
+        End Sub
     End Class
     Class LinkedList(Of T As IComparable)
         Dim head As Integer = -1
-        Dim _heapStart As Integer = 0
+        Dim heap As Integer = -1
+        Dim free As Integer = -1
         Dim _cap As Integer = 10
         Dim length As Integer = 0
-        Dim lastInsertNext As Integer = 0
+
         Dim Data(_cap - 1) As Node(Of T)
-        Function Contains(item As T) As Boolean
-            Dim _head = head
-            While _head >= 0 AndAlso Not Data(_head).data.Equals(item)
-                _head = Data(_head)._next
-            End While
-            Return _head < 0
-        End Function
-        Sub InsertAt(item As T, index As Integer)
-            '''If inserting before b
-            ''' |a| -> |b| -> |c| , save pointer from a to b, set pointer from a to inserted item, set pointer of inserted item to saved b
-            ''' |b| -> |c| set pointer from saved pointer to b, set head as pointer to inserted item
-            ''' |a| -> |b| 
-            Add(item)
-            Dim after = Data(index)._next
-            Data(index)._next = lastInsertNext - 1
-
-
+        Sub Resize(newSize As Integer)
+            ReDim Preserve Data(newSize)
+            _cap = newSize
         End Sub
+        ''' <summary>
+        ''' Add item to the front of the list
+        ''' </summary>
+        ''' <param name="item"></param>
         Sub Add(item As T)
-
-
+            Dim node = New Node(Of T)(item)
             If head = -1 Then
+                Data(0) = node
                 head = 0
-                Data(head) = New Node(Of T)(item, 0)
-                lastInsertNext = 1
+                heap = 1
                 length = 1
                 Return
             End If
 
-            If lastInsertNext = _cap Then
-                ReDim Preserve Data(2 * _cap)
-                _cap *= 2
-            End If
-            Data(lastInsertNext) = New Node(Of T)(item, lastInsertNext + 1)
-            lastInsertNext += 1
-            length += 1
-        End Sub
-        Function Nth(n As Integer) As Node(Of T)
-            Return Data(n)
-        End Function
+            node._next = head
+            Data(heap) = node
+            head = heap
+            heap += 1
 
+
+        End Sub
     End Class
 End Module
